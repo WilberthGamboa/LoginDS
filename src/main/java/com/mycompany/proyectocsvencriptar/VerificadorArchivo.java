dc9293c9b1d6e6e24af2277312dff452d22569c0
Tamajire <wgcampos15@gmail.com>
2022-03-08 10:47:19 -0600

    Paneles en verificar

--- src/main/java/com/mycompany/proyectocsvencriptar/VerificadorArchivo.java
+++ src/main/java/com/mycompany/proyectocsvencriptar/VerificadorArchivo.java
@@ -1,20 +1,50 @@
 package com.mycompany.proyectocsvencriptar;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class VerificadorArchivo {
     private boolean flag;
 
-
-    public boolean getVerificarExistenciaArchivo(){
-
-        return flag;
-       
-      
+    public VerificadorArchivo() {
+        this.flag = false;
     }
 
+    public boolean getVerificarExistenciaArchivo() {
 
-    public void setVerificarExistenciaArchivo(boolean flag){        
+        return flag;
 
-        this.flag=flag;
-        
+    }
+
+    public void setVerificarExistenciaArchivo() {
+        JFileChooser seleccionarArchivo = new JFileChooser();
+        FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos txt", "txt");
+        seleccionarArchivo.setFileFilter(filtro);
+        int result = seleccionarArchivo.showDialog(new JFrame(), "Abrir txt");
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File archivo = seleccionarArchivo.getSelectedFile();
+            if (archivo.getName().endsWith("usuarios.txt")) {
+                this.flag = true;
+
+                // ESTA PARTE DE AQUI ESTÁ RARA SE DEBE CAMBIAR, POR QUE ESTAMOS USANDO LA
+                // VARIABLE SISTEMA
+
+                // JOptionPane.showMessageDialog(null, "Se encontró");
+
+            } 
+
+        } else {
+
+            /*
+             * JOptionPane.showMessageDialog(null,
+             * "Archivo no encontrado, el programa no se cerrará");
+             * System.exit(0);
+             */
+        }
+
     }
 }
